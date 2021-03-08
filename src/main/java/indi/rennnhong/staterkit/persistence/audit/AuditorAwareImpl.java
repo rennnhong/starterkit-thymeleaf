45034992f4d31b49a96d4bd5b16497153f5bf663
MSI\User <sunkistfool@gmail.com>
2021-03-08 17:23:19 +0800

    去除空行

--- src/main/java/indi/rennnhong/staterkit/persistence/audit/AuditorAwareImpl.java
+++ src/main/java/indi/rennnhong/staterkit/persistence/audit/AuditorAwareImpl.java
@@ -1,7 +1,6 @@
 package indi.rennnhong.staterkit.persistence.audit;
 
 import org.springframework.data.domain.AuditorAware;
-
 import java.util.Optional;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
